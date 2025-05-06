e712fec324996ed2999843b8e059921af739d10f
Nickyfan7 <65929380+Nickyfan7@users.noreply.github.com>
2025-05-06 15:41:14 -0400

    Website update
    Updated personal website added cosmology.

---
+++ personal_website/scripts.js
@@ -0,0 +1,12 @@
+<script type = "text/javascript">
+				var timeDisplay = document.getElementById("time");
+
+
+				function refreshTime() {
+					var dateString = new Date().toLocaleString("en-US", {timeZone: "America/Sao_Paulo"});
+					var formattedString = dateString.replace(", ", " - ");
+					timeDisplay.innerHTML = formattedString;
+				}
+
+				setInterval(refreshTime, 1000);
+</script> 
